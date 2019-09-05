4ffe08971040fb2ab9b086b80654386b70947d03
aokitashipro <webmocha2@gmail.com>
2019-09-06 06:47:21 +0900

    glslify追加

---
+++ README.md
@@ -0,0 +1,20 @@
+Three.js練習用テンプレートです。
+
+Three.jsはCDNで取り込むようにしています。
+
+git clone https://github.com/aokitashipro/threejs.git
+
+# node_module群をインストールします。
+npm install
+
+#開発ビルド
+npm run dev
+
+#監視
+npm run watch
+
+#開発サーバー起動
+npm run start
+
+#本番ビルド
+npm run build
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,35 @@
-module.exports = {
-  // モード値を production に設定すると最適化された状態で、
-  // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: 'production',
+const path = require('path');
+
+module.exports = (env, argv) => ({
+  // モードが production だと最適化された状態で、
+  // development だとソースマップ有効でJSファイルが出力される
 
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: './src/index.js',
   // ファイルの出力設定
   output: {
-    //  出力ファイルのディレクトリ名
-    path: `${__dirname}/dist`,
     // 出力ファイル名
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    //  出力ファイルのディレクトリ名
+    path: path.resolve(__dirname, 'dist')
+
   },
+  //監視有効
+  watch: true,
+  //開発用サーバー
+  devServer:{
+    contentBase: path.resolve(__dirname, 'dist'),
+    watchContentBase: true,
+    //ポート指定
+    port: 3000,
+  },
+
   module: {
     rules: [
       {
         // 拡張子 .js の場合
         test: /\.js$/,
+        // node_modulesは対象外にしておく
         exclude: /node_modules/,
         use: 
           {
@@ -33,10 +46,10 @@ module.exports = {
       },
       { // 拡張子 .glsl .vert .frag の場合
         test: /\.(glsl|vert|frag)$/,
-        // raw-loader と glslify-loaderを利用する
-        loader: [ 'raw-loader', 'glslify-loader' ]
+        // webpack向けglsl-loaderを利用する
+        loader: [ 'raw-loader', 'webpack-glsl-loader' ]
       }
     ]
 
   }
-};
+});
